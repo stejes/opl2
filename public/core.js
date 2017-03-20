52985a1a68ac7e4adebab52e03509b804156ebb0
StevenJ <s.jespers@telenet.be>
2017-03-20 14:15:57 +0100

    filter cursisten op opleidingen

--- public/core.js
+++ public/core.js
@@ -22,6 +22,27 @@ oplApp.config(function ($routeProvider) {
             .otherwise({redirectTo: '/'});
 });
 
+oplApp.filter('filterOpl', function(){
+    return function(input, code){
+        if(!code || code == ""){
+            return input;
+        }
+        
+        var i,j;
+        var returnArr = new Array();
+        
+        for(i=0; i<input.length;i++){
+            for(j=0; j< input[i].opleidingen.length;j++){
+                if(input[i].opleidingen[j].opleidinginfo.oplCode == code){
+                    returnArr.push(input[i]);
+                }
+            }
+            
+        }
+        return returnArr;
+    };
+})
+
 oplApp.controller('mainController', ['$scope', '$http', function ($scope, $http) {
         $scope.formData = {};
 
